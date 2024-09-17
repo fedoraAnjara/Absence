f3591eb7156be28efcc798e5c10608d90bdc837a
fedoraAnjara <hei.fedora.3@gmail.com>
2024-09-17 01:10:00 -0400

    chore: initialise spring project

---
+++ presenceBackend/src/main/java/com/fedoraa/presencebackend/PresenceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fedoraa.presencebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresenceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PresenceBackendApplication.class, args);
+	}
+
+}
---
+++ presenceBackend/src/test/java/com/fedoraa/presencebackend/PresenceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fedoraa.presencebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PresenceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
